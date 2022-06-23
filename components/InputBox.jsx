30987165bebf89df0b1b325c8605c1770be73d77
phichayut-pak <pakkung1245@gmail.com>
2022-06-23 12:54:08 +0700

    fixed InputBox

--- components/InputBox.jsx
+++ components/InputBox.jsx
@@ -64,10 +64,10 @@ const InputBox = ({ className, onSubmitLink }) => {
 
   return (
     <div id="input_bg" className={`${classes.input_bg} absolute left-0 right-0 ml-auto mr-auto w-11/12 lg:w-3/4 -bottom-[4.5rem] lg:-bottom-[5.5rem] py-5 lg:py-16 bg-dark-violet px-7 lg:px-16 rounded-md`}>
-      <form onSubmit={submitHandler} className='flex flex-col lg:flex-row justify-start items-center space-x-0 space-y-3 lg:space-x-3 lg:space-y-0'>
+      <form onSubmit={submitHandler} className='relative flex flex-col lg:flex-row justify-start items-center space-x-0 space-y-3 lg:space-x-3 lg:space-y-0'>
 
         <input id="input_link" ref={link} type="text" className={`w-full lg:w-9/12 h-12 rounded-md px-5 font-poppins font-medium text-gray placeholder:font-poppins placeholder:font-medium placeholder:text-gray text-lg placeholder:text-lg outline-none ${!isValid.status ? 'border-2 border-red' : ''}`} placeholder='Shorten a link here...'/>
-        <label htmlFor="input_link" className={`font-poppins font-medium text-xs text-red w-full italic ${!isValid.status ? 'block' : 'hidden'}`}>{isValid.message}</label>
+        <label htmlFor="input_link" className={`lg:absolute lg:-bottom-5 lg:-left-3 font-poppins font-medium text-xs text-red w-full italic ${!isValid.status ? '' : 'hidden'}`}>{isValid.message}</label>
         
         <div className='w-full lg:w-3/12 bg-cyan h-12 rounded-md transition duration-100 ease-in hover:bg-[#9BE3E2]'>
           <button type="submit" onClick={submitHandler} className='inline-flex justify-center items-center w-full h-full font-poppins font-bold text-white text-lg'>Shorten It!</button>
